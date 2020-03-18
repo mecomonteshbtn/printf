2cb44483669bfe3d8a520acb11619b5ce3637dfc
Robinson Montes <1574@holbertonschool.com>
2020-03-18 19:56:53 +0000

    some advanced projects

---
+++ print_R.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * print_R - A function that prints a string in rot13 format
+ * @R: string to print
+ * Return: number of printed characters
+ */
+int print_R(va_list R)
+{
+	char *str;
+	unsigned int i = 0, j = 0;
+	int counter = 0;
+	char in[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char out[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	str = va_arg(R, char *);
+	if (str == NULL)
+		str = "(ahyy)";
+	for (; str[i]; i++)
+	{
+		for (j = 0; in[j]; j++)
+		{
+			if (in[j] == str[i])
+			{
+				_putchar(out[j]);
+				counter++;
+				break;
+			}
+		}
+		if (!in[j])
+		{
+			_putchar(str[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
